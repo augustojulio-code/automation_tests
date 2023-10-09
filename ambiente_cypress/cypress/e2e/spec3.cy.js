7cd44de2c12dea4d146b57fa2843e51af5a0dbf4
Julio <julio.mech@hotmail.com>
2023-10-09 14:08:12 -0300

    tests

--- ambiente_cypress/cypress/e2e/spec3.cy.js
+++ ambiente_cypress/cypress/e2e/spec3.cy.js
@@ -1,6 +1,7 @@
 let num = 10;
 const num2 = 10;
 var num3 = 10;
+var nome = "Olá imbecil";
 
 describe('Test expect', () => {
 
@@ -9,10 +10,15 @@ describe('Test expect', () => {
     })
 
     it('Pass with const', function () {
-        expect(5 + 5).to.equal(num)
+        expect(5 + 5).to.equal(num2)
     })
 
     it('Pass with var', function () {
-        expect(5 + 5).to.equal(num)
+        expect(5 + 5).to.equal(num3)
     })
+
+    it('Pass with string', () => {
+        expect("Olá imbecil").to.equal(nome)
+    })
+
 })
