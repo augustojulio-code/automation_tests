9358d8d1aaa97fd533f211067dde772e1732a60c
Julio <julio.mech@hotmail.com>
2023-10-09 14:17:58 -0300

    tests

--- ambiente_cypress/cypress/e2e/spec3.cy.js
+++ ambiente_cypress/cypress/e2e/spec3.cy.js
@@ -2,6 +2,7 @@ let num = 10;
 const num2 = 10;
 var num3 = 10;
 var nome = "Olá imbecil";
+var tfvar = false;
 
 describe('Test expect', () => {
 
@@ -19,6 +20,10 @@ describe('Test expect', () => {
 
     it('Pass with string', () => {
         expect("Olá imbecil").to.equal(nome)
+    })
+
+    it('Pass boolean var', () => {
+        expect(false).to.equal(tfvar)
     })
 
 })
