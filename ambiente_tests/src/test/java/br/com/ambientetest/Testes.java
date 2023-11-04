bc6e8815f23585d2ed21ab1a61e1bfda833a6f36
Julio <julio.mech@hotmail.com>
2023-11-04 08:33:56 -0300

    testing tests

--- ambiente_tests/src/test/java/br/com/ambientetest/Testes.java
+++ ambiente_tests/src/test/java/br/com/ambientetest/Testes.java
@@ -2,12 +2,28 @@ package br.com.ambientetest;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Testes {
 
-    @Test
-    public void testeComParametro() {
-        assertEquals(true, true);
+    @Before
+    public void setUp() {
+
+        System.setProperty("webdriver.chrome.driver", "src\\drive\\chrome.exe");
+
     }
+
+    @Test
+    public void openPage() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.google.com.br/");
+    }
+
 }
